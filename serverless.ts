cba4cd251acca2071bffbec2bf96cfd319393f0e
David Behar <davidbehar@Davids-MacBook-Pro.local>
2022-05-02 09:57:03 -0500

    Create element in dynamoDb

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,10 @@ import auctionFns from '@functions/auction';
 const serverlessConfiguration: AWS = {
   service: 'ts-base-project',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: [
+    'serverless-esbuild',
+    'serverless-pseudo-parameters'
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -19,6 +22,41 @@ const serverlessConfiguration: AWS = {
     },
     stage: "${opt:stage, 'dev'}",
     region: "${opt:region, 'us-east-1'}" as "us-east-1",
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: [
+          'dynamodb:PutItem',
+        ],
+        Resource: [
+          'arn:aws:dynamodb:#{AWS::Region}:#{AWS::AccountId}:table/AuctionsTable',
+        ]
+      }
+    ]
+  },
+  resources:{
+    // cloud formation syntax
+    Resources: {
+      AuctionsTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'AuctionsTable',
+          BillingMode: 'PAY_PER_REQUEST',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'id',
+              AttributeType: 'S', // S: string | N: number | B: binary
+            }
+          ],
+          KeySchema: [
+            { // partition key id of type Hash
+              AttributeName: 'id',
+              KeyType: 'HASH', // HASH | RANGE
+            }
+          ],
+        }
+      }
+    }
   },
   // import the function via paths
   functions: {
--- src/functions/auction/createAuctionHandler.ts
+++ src/functions/auction/createAuctionHandler.ts
@@ -1,18 +1,30 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
 import schema from './CreateAuctionSchema';
+
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
 
 const createAuction: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event, context) => {
   const { title } = event.body;
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
   }
+
+  await dynamoDb.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
+
   return formatJSONResponse({
     auction,
     event,
