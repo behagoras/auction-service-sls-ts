1a1841e1e214e901388865b8c72993a39e89e2f8
David Behar <david.behar@improving.com>
2022-05-16 16:59:09 -0600

    Refactor services in handlers

--- src/functions/auction/getAuctions/getAuctionsHandler.ts
+++ src/functions/auction/getAuctions/getAuctionsHandler.ts
@@ -1,26 +1,13 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@Libs/api-gateway';
 import { formatJSONResponse } from '@Libs/api-gateway';
 import { middyfy } from '@Libs/lambda';
-import { DynamoDB } from 'aws-sdk';
-import { InternalServerError } from 'http-errors';
+import { getAuctionsService } from '@Services/auction.services';
 import GetAuctionsSchema from './GetAuctionsSchema';
-
-const dynamoDb = new DynamoDB.DocumentClient();
 
 const getAuctions: ValidatedEventAPIGatewayProxyEvent<typeof GetAuctionsSchema> = async () => {
   console.log('getAuctions handler');
-  const TableName = process?.env?.AUCTIONS_TABLE_NAME!;
-  let auctions: DynamoDB.DocumentClient.ItemList | undefined
 
-  try {
-    const result = await dynamoDb.scan({
-      TableName,
-    }).promise()
-    auctions = result.Items
-  } catch (error) {
-    console.error(error);
-    throw new InternalServerError(JSON.stringify(error));
-  }
+  const auctions = await getAuctionsService()
 
   return formatJSONResponse({
     auctions,
